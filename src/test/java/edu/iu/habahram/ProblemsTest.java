a038c2b478ad1ce0ae51f346e2fdb793f3710acc
hbahramian <habahram@iu.edu>
2024-01-14 19:11:12 -0500

    .

--- src/test/java/edu/iu/habahram/ProblemsTest.java
+++ src/test/java/edu/iu/habahram/ProblemsTest.java
@@ -9,11 +9,11 @@ class ProblemsTest {
     @Test
     void problem1_Test1() {
         int[] numbers = {2, 7, 11, 15};
-        var target = 9;
+        int target = 9;
         int[] expected = {0, 1};
 
-        var problems = new Problems();
-        var output = problems.problem1(numbers, target);
+        Problems problems = new Problems();
+        int[] output = problems.problem1(numbers, target);
 
         assertArrayEquals(expected, output);
     }
@@ -21,11 +21,11 @@ class ProblemsTest {
     @Test
     void problem1_Test2() {
         int[] numbers = {3, 2, 4};
-        var target = 6;
+        int target = 6;
         int[] expected = {1, 2};
 
-        var problems = new Problems();
-        var output = problems.problem1(numbers, target);
+        Problems problems = new Problems();
+        int[] output = problems.problem1(numbers, target);
 
         assertArrayEquals(expected, output);
     }
@@ -33,11 +33,11 @@ class ProblemsTest {
     @Test
     void problem1_Test3() {
         int[] numbers = {3, 3};
-        var target = 6;
+        int target = 6;
         int[] expected = {0, 1};
 
-        var problems = new Problems();
-        var output = problems.problem1(numbers, target);
+        Problems problems = new Problems();
+        int[] output = problems.problem1(numbers, target);
 
         assertArrayEquals(expected, output);
     }
@@ -45,11 +45,11 @@ class ProblemsTest {
     @Test
     void problem1_Test4() {
         int[] numbers = {3, 3};
-        var target = 0;
+        int target = 0;
         int[] expected = {-1, -1};
 
-        var problems = new Problems();
-        var output = problems.problem1(numbers, target);
+        Problems problems = new Problems();
+        int[] output = problems.problem1(numbers, target);
 
         assertArrayEquals(expected, output);
     }
@@ -57,9 +57,9 @@ class ProblemsTest {
     @Test
     void problem1_Test5() {
         int[] numbers = {3};
-        var target = 3;
+        int target = 3;
 
-        var problems = new Problems();
+        Problems problems = new Problems();
         assertThrows(IllegalArgumentException.class,
                 () -> {
                     problems.problem1(numbers, target);
